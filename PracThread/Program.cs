5056fc23d712400d6fb8d43471bdfbac072202b6
Pengu <77952344+kg4543@users.noreply.github.com>
2022-04-16 14:06:49 +0900

    Practice
    연습파일 생성

---
+++ PracThread/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Threading;
+
+namespace PracThread
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
