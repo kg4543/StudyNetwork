f3d87ad44ffd2e53f1b0d797f6da7f1cdbd6461e
Pengu <77952344+kg4543@users.noreply.github.com>
2022-04-16 18:30:51 +0900

    Lock
    DeadLock / SpinLock

---
+++ Lock/Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Lock
+{
+    class SpinLock
+    {
+        //가시성 보장(volatile)
+        volatile int _locked = 0;
+
+        public void Acquire()
+        {
+            while (true)
+            {
+                /*int origin = Interlocked.Exchange(ref _locked, 1);
+
+                if (origin == 0) // 잠금이 풀려 대기에서 벗어남
+                    break;*/
+
+                //CAS = Compare And Swap
+
+                int expected = 0; // 기대값
+                int desire = 1; // 원하는 값
+                //_locked가 잠금이 안 잠겨있으면 (_locdked == expected) _locked를 잠근다. (_locked = desire)
+                if(Interlocked.CompareExchange(ref _locked, desire, expected) == expected)
+                    break;
+
+                // 쉬다오기
+                //Thread.Sleep(10); // 무조건 휴식
+                //Thread.Sleep(0); // 조건부 양보 => 우선순위가 나보다 같거나 높은 쓰레드가 없으면 다시 본인(대기)
+                Thread.Yield(); // 관대한 양보 => 지금 실행이 가능한 쓰레드가 있으면 실행 / 없으면 다시 본인(대기)
+            }
+        }
+
+        public void Release()
+        {
+            _locked = 0;
+        }
+    }
+
+    class Program
+    {
+        static int num = 0;
+        static SpinLock spinLock = new SpinLock();
+
+        static void Thread1()
+        {
+            for (int i = 0; i < 100000; i++)
+            {
+                spinLock.Acquire();
+                num++;
+                spinLock.Release();
+            }
+        }
+
+        static void Thread2()
+        {
+            for (int i = 0; i < 100000; i++)
+            {
+                spinLock.Acquire();
+                num--;
+                spinLock.Release();
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            Task t1 = new Task(Thread1);
+            Task t2 = new Task(Thread2);
+            t1.Start();
+            t2.Start();
+
+            Task.WaitAll(t1, t2);
+            Console.WriteLine(num);
+        }
+    }
+}
--- PracThread/Program.cs
+++ PracThread/Program.cs
@@ -1,14 +1,143 @@
 ﻿using System;
 using System.Threading;
+using System.Threading.Tasks;
 
 namespace PracThread
 {
+    class FastLock
+    {
+        int id;
+    }
 
+    class SessionManager
+    {
+        static object _lock = new object();
+
+        public static void TestSession()
+        {
+            lock(_lock)
+            {
+
+            }
+        }
+
+        public static void Test()
+        {
+            lock(_lock)
+            {
+                UserManager.TestUser();
+            }
+        }
+    }
+
+    class UserManager
+    {
+        static object _lock = new object();
+
+        public static void TestUser()
+        {
+            lock (_lock)
+            {
+
+            }
+        }
+
+        public static void Test()
+        {
+            lock (_lock)
+            {
+                SessionManager.TestSession();
+            }
+        }
+    }
+
+    // 경합조건
     class Program
     {
+        static int num;
+        static object _obj = new object();
+        
+
+        static void Thread1()
+        {
+            for (int i = 0; i < 100000; i++)
+            {
+                //num++;
+
+                //-> 어셈블리형으로 쪼개면...
+                /*int temp = num;
+                temp += 1;
+                num = temp;*/
+
+                // 원자성(Atomic)
+                // 각 동작이
+                // 한번에 이루어져야한다.
+                // 유저끼리 아이템 교환 등 (트랜잭션 / 커밋)
+
+                // All or Nothing
+                // 원자성이 보장하나 성능이 다운
+                //Interlocked.Increment(ref num);
+
+                //상호배제
+                //먼저 점유할 경우 다음 차례는 대기
+
+
+                /*try
+                {
+                    Monitor.Enter(_obj); //잠금
+                    num++;
+
+                    return;
+                    //중간에 return을 할 경우 잠금이 풀리지 않아 Thread2가 무한대기상황(DeadLock)이 될 수 있다 
+                }
+                catch (Exception ex)
+                {
+
+                }
+                finally
+                {
+                    Monitor.Exit(_obj); //잠금해제
+                }*/
+
+                /*lock(_obj)
+                {
+                    num++;
+                }*/
+
+                SessionManager.Test();
+            }
+        }
+
+        static void Thread2()
+        {
+            for (int i = 0; i < 100000; i++)
+            {
+                //num--;
+                //원자성
+                //Interlocked.Decrement(ref num);
+
+                /*Monitor.Enter(_obj);
+                num--;
+                Monitor.Exit(_obj);*/
+
+                /*lock (_obj)
+                {
+                    num--;
+                }*/
+
+                UserManager.Test();
+            }
+        }
+
         static void Main(string[] args)
         {
-            
+            Task task1 = new Task(Thread1);
+            Task task2 = new Task(Thread2);
+            task1.Start();
+            task2.Start();
+
+            Task.WaitAll(task1, task2);
+            Console.WriteLine(num);
         }
     }
 }
